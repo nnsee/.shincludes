e98bf599bf69a329fd2d8bba93ba0060b98f8ada
Rasmus Moorats <me@neonsea.uk>
2020-01-27 16:01:28 +0200

    can specify multiple wireguard interfaces

--- networking/wireguard.sh
+++ networking/wireguard.sh
@@ -1,7 +1,19 @@
 wgu() {
-    sudo wg-quick up "${@}-wg"
+    for interface in "$@"; do
+        sudo wg-quick up "${interface}-wg"
+    done
 }
 
 wgd() {
-    sudo wg-quick down "${@}-wg"
+    if [[ -z "${@}" ]]; then
+        # bring all down
+        for interface in $(wg show interfaces); do
+            sudo wg-quick down $interface
+        done
+        return
+    fi
+
+    for interface in "$@"; do
+        sudo wg-quick down "${interface}-wg"
+    done
 }
