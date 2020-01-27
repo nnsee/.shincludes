c041bfbadaf49bacdbef08950e2ac35d19d37541
Rasmus Moorats <me@neonsea.uk>
2020-01-27 10:41:13 +0200

    display hostname when SSH'd in

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,10 +1,19 @@
 # virtualenv prompt prompt
 
+HOSTNAME=$(hostname)
+
 function virtual_env_prompt () {                                                                      
     REPLY=${VIRTUAL_ENV+(${VIRTUAL_ENV:t}) }                
 }
 
 grml_theme_add_token virtual-env -f virtual_env_prompt '%F{magenta}' '%f'
+
+function ssh_conn_prompt () {                                                                      
+    REPLY=${SSH_CONNECTION+@${HOSTNAME:t} }                
+}
+
+grml_theme_add_token ssh-conn -f ssh_conn_prompt '%F{white}' '%f'
+
 
 # change git display
 
@@ -15,4 +24,4 @@ zstyle ':vcs_info:*' unstagedstr '!'
 zstyle ':vcs_info:*' stagedstr '+'
 zstyle ':vcs_info:git*' formats "%{${fg[cyan]}%}[%{${fg[blue]}%}%b%{${fg[yellow]}%}%m%u%c%{${fg[cyan]}%}]%{$reset_color%} "
 
-zstyle ':prompt:grml:left:setup' items rc virtual-env change-root path vcs percent
+zstyle ':prompt:grml:left:setup' items rc ssh-conn virtual-env change-root path vcs percent
