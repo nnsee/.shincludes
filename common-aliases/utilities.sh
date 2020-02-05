50ea5cddb9d858a71539c5e42b040c2cff3dc54c
Rasmus Moorats <me@neonsea.uk>
2020-02-05 15:44:12 +0200

    add bcat

--- common-aliases/utilities.sh
+++ common-aliases/utilities.sh
@@ -4,3 +4,14 @@ wd() {
     . $HOME/.local/share/wd/wd.sh
 }
 
+bcat() {
+    # cat a binary file
+    for bin in "$@"; do
+        LOCATION=$(/usr/bin/which --skip-functions "$bin" 2>/dev/null)
+        if [[ -z "$LOCATION" ]]; then
+            echo "no such bin: $bin" 1>&2
+        else
+            cat "$LOCATION"
+        fi
+    done
+}
