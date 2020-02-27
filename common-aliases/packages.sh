da586f12fb760affe1ee3712451179cb954a216e
Rasmus Moorats <me@neonsea.uk>
2020-02-27 17:13:38 +0200

    added ip coloring

--- common-aliases/packages.sh
+++
@@ -1,5 +0,0 @@
-# depends: yay
-
-alias i='yay -S'
-alias up='yay -Syu'
-alias whoowns='yay -Qo'
---
+++ common-aliases/simple-aliases.sh
@@ -0,0 +1,7 @@
+# depends: yay
+
+alias i='yay -S'
+alias up='yay -Syu'
+alias whoowns='yay -Qo'
+
+alias ip='ip -h -c' # adds some colour
