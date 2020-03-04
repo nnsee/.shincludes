9c67462f7c20f02c4af5d7304fa5ad017d428533
Rasmus Moorats <me@neonsea.uk>
2020-03-04 13:21:58 +0200

    add saved alias

--- common-aliases/simple-aliases.sh
+++ common-aliases/simple-aliases.sh
@@ -5,3 +5,10 @@ alias up='yay -Syu'
 alias whoowns='yay -Qo'
 
 alias ip='ip -h -c' # adds some colour
+
+saved() {
+    # my Downloads is a tmpfs
+    for file in $@; do
+        mv "$HOME/Downloads/$file" "$HOME/SavedDownloads/$file"
+    done
+}
