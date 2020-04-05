cbae43ad229b3f911777f43829e66a8c62c4cdb4
Rasmus Moorats <me@neonsea.uk>
2020-04-05 13:02:50 +0300

    disable automatic pyenv init, causes shell sluggishness

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -1,5 +1,5 @@
 # depends: pyenv
 # install note: "curl https://pyenv.run | bash"
 export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
