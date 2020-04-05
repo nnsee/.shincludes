d6f030f44fadc31db9858fd6c7d7565ac870118b
Rasmus Moorats <me@neonsea.uk>
2020-04-05 13:07:16 +0300

    create alias to activate pyenv and plugins

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -3,3 +3,8 @@
 export PATH="$HOME/.pyenv/bin:$PATH"
 # eval "$(pyenv init -)"
 # eval "$(pyenv virtualenv-init -)"
+
+pyenv-init() {
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+}
