6ae5064df5f9374048d71b97f63552215f7b44d9
Rasmus Moorats <me@neonsea.uk>
2020-06-08 17:30:10 +0300

    add bgr alias

--- common-aliases/simple-aliases.sh
+++ common-aliases/simple-aliases.sh
@@ -3,6 +3,7 @@
 alias i='yay -S'
 alias up='yay -Syu'
 alias whoowns='yay -Qo'
+alias ownswhom='yay -Ql'
 
 alias ip='ip -h -c' # adds some colour
 alias ipa='ip -br a' # brief IP listing
@@ -15,6 +16,12 @@ saved() {
     for file in $@; do
         mv "$HOME/Downloads/$file" "$HOME/SavedDownloads/$file"
     done
+}
+
+bgr() {
+    # run in background, silence everything
+    nohup $@ 2>&1 > /dev/null &
+    disown
 }
 
 alias clear-logs='sudo /bin/sh -c "journalctl --rotate && journalctl --vacuum-time=1s"'
