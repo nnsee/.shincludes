79f0d372fcbf98d1a0a18864b63d474fc7010669
Rasmus Moorats <xx@nns.ee>
2021-04-13 15:55:47 +0300

    update title

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -30,3 +30,18 @@ zstyle ':vcs_info:git*' formats "%{${fg[cyan]}%}[%{${fg[blue]}%}%b%{${fg[yellow]
 zstyle ':prompt:grml:left:setup' items rc ssh-conn virtual-env change-root path vcs percent
 zstyle ':prompt:grml:right:setup' use-rprompt false
 RPS1=''
+
+function set_title_precmd () {
+    set_title ${(%):-"%~"}
+}
+
+# Sets the title as name of program currently running
+function set_title_preexec () {
+    set_title "${(%):-}" "$2"
+}
+
+if [[ "$TERM" == (alacritty*|gnome*|konsole*|putty*|rxvt*|screen*|tmux*|xterm*) ]]; then
+    autoload -Uz add-zsh-hook
+    add-zsh-hook -Uz precmd set_title_precmd
+    add-zsh-hook -Uz preexec set_title_preexec
+fi
