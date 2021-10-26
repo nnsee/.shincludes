70a018a46b5d3920a6c50a3638dd8a13e8767021
Rasmus Moorats <xx@nns.ee>
2021-10-26 16:00:55 +0300

    update screenshot script

--- common-aliases/screenshot.sh
+++ common-aliases/screenshot.sh
@@ -1,16 +1,4 @@
-# depends: maim wl-clipboard curl
-# optional dep: libnotify
-
-randchars () {
-    < /dev/urandom tr -dc 'a-zA-Z0-9' | head -c $1
-}
-
-file_exists () {
-    curl "$1" -f -s -o /dev/null && \
-      return 0 # file exists
-
-    return 1 # file does not exist
-}
+# depends: grim slurp imagemagick imv wl-clipboard curl
 
 local_screenshot_old () {
     local FILENAME=/home/xx/Pictures/ss/$(date '+%F-%H-%M-%S').png
@@ -22,8 +10,6 @@ local_screenshot () {
     # pauses the screen when doing a selection
     # requires the following line in sway config to work properly
     # for_window [title="imv.*screenshot"] fullscreen global
-    # also requires a patched sway due to a bug in cursor detection
-    # see: https://github.com/swaywm/sway/pull/6545
     local FILENAME=/home/xx/Pictures/ss/$(date '+%F-%H-%M-%S').png
     local screenshot="$(mktemp --suffix screenshot.ppm)"
     grim -c -t ppm "${screenshot}"
