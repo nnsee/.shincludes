b7b7211152ca6836d589872762b61849ba62f73a
Rasmus Moorats <xx@nns.ee>
2021-10-28 15:07:26 +0300

    add whatprovides for ubuntu

--- common-aliases/simple-aliases.sh
+++ common-aliases/simple-aliases.sh
@@ -1,19 +1,26 @@
 [ -z "${ID+x}" ] && source /etc/os-release
 
 # depends: paru
-[ "${ID}" = "arch" ] || [ "${ID_LIKE}" = "arch" ] && \
-    alias i='paru -S' && \
-    alias up='paru -Syu' && \
-    alias whoowns='paru -Qo' && \
-    alias ownswhom='paru -Ql' && \
+{[ "${ID}" = "arch" ] || [ "${ID_LIKE}" = "arch" ]} && {
+    alias i='paru -S'
+    alias up='paru -Syu'
+    alias whoowns='paru -Qo'
+    alias ownswhom='paru -Ql'
     alias whatprovides='paru -F'
+}
 
-[ "${ID}" = "ubuntu" ] || [ "${ID}" = "debian" ] && \
-    alias i='sudo apt install' && \
-    alias up='sudo apt update && sudo apt -y full-upgrade' && \
-    alias whoowns='dpkg -S' && \
-    alias ownswhom='dpkg -L' # && \
-#    alias whatprovides='' # not implemented on ubuntu yet
+# depends: apt-file
+{[ "${ID}" = "ubuntu" ] || [ "${ID}" = "debian" ]} && {
+    alias i='sudo apt install'
+    alias up='sudo apt update && sudo apt -y full-upgrade'
+    alias whoowns='dpkg -S'
+    alias ownswhom='dpkg -L'
+    eval whatprovides() {
+        for file in $@; do
+            apt-file find -x "/${file}\$"
+        done
+    }
+}
 
 alias bthp='bluetoothctl power on; bluetoothctl connect 94:DB:56:6B:E3:8A'
 
