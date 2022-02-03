c3b123067cdfcbefeef5543add99cd1da9d60d80
Rasmus Moorats <xx@nns.ee>
2022-02-03 11:23:09 +0200

    don't let golang dump stuff into $HOME

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 whence -p nvim > /dev/null && export EDITOR=nvim || true
 
+# don't let golang dump stuff into $HOME
+export GOMODCACHE="$HOME/.cache/go-mod"
+export GOPATH="$HOME/.go"
+export PATH="$PATH:$HOME/.go/bin"
+
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.nimble/bin"
